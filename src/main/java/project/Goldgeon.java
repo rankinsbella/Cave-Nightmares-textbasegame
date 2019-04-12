53146a9614f6590b6f1b11f5f92274e27cca05e1
Andrew Rankins-Bell <rankinsbella@aldenv170.allegheny.edu>
2019-04-12 11:40:16 -0400

    Did a little more work with project.

--- src/main/java/project/Goldgeon.java
+++ src/main/java/project/Goldgeon.java
@@ -2,29 +2,30 @@ package project;
 
 import java.util.Random;
 import java.util.Scanner;
-import java.io.IOExecption;
+
 
 public class Goldgeon {
 
   public static void main(String[] args) {
   //creating instance of variables
-    Scannner scan = new scan(System.in);
-    Random generator = new random();
+    Scanner scan = new Scanner(System.in);
+    Random generator = new Random();
 
   //introduction to your adventure
-  System.out.println("Hello explorer, while sailing the 7 seas your ship took
-    on too much water.");
-  System.out.println("All of your crew members died at sea and you are the only
-    surviver.")
+  System.out.println("Hello explorer, while sailing the 7 seas your ship took"
+      + "on too much water.");
+  System.out.println("All of your crew members died at sea and you are the only"
+      + "surviver.");
   System.out.println("This is your story.");
 
   //character creation
   System.out.println("Choose your gender");
-  Gender gen1 = new Gender("male");
-  Gender gen2 = new Gender("female");
-  //
 
+  //choosen gender
+  String choice = scan.nextLine();
+  Player one = new Player(choice);
 
+    
   }
 
 }
---
+++ src/main/java/project/Player.java
@@ -0,0 +1,10 @@
+package project;
+//Constructor for gender
+public class Player {
+String gender;
+  public Player(String G) {
+  this.gender = G;
+
+  }
+
+}
